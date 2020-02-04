5cb89fc0b76484c15b3f6ceacbc51eb70a6731f8
ga7uti <gauti783@gmail.com>
2020-02-04 12:30:45 +0530

    Completed Lab6

--- src/com/lab6/Exercise10.java
+++ src/com/lab6/Exercise10.java
@@ -1,5 +1,40 @@
 package com.lab6;
 
+import java.util.Scanner;
+
 public class Exercise10 {
 
+	public static boolean validateUserName(String userName) {
+
+		if (userName.isEmpty()) {
+			System.out.println("Username cannot be blank");
+			return false;
+		} else if (!userName.contains("_job")) {
+			System.out.println("Username must contain '_job' ");
+			return false;
+		} else {
+			int index = userName.lastIndexOf('_');
+			if (!(index + 3 == userName.length() - 1)) {
+				System.out.println("Username doesnot contains '_job' at the end");
+				return false;
+			} else if (userName.substring(0, index).length() < 8) {
+				System.out.println("Username length should be greater than 8 excluding '_job'");
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your user name : ");
+		String userName = scanner.nextLine();
+		if (validateUserName(userName)) {
+			System.out.println("Username validated");
+		}
+		scanner.close();
+
+	}
 }
--- src/com/lab6/Exercise9.java
+++ src/com/lab6/Exercise9.java
@@ -9,30 +9,31 @@ import java.util.Scanner;
 public class Exercise9 {
 
 	public static void diffInDate(Date inputDate, Date currentDate) {
-		long diff = currentDate.getTime() - inputDate.getTime();
-		long diffSeconds = diff / 1000 % 60;
-		long diffMinutes = diff / (60 * 1000) % 60;
-		long diffHours = diff / (60 * 60 * 1000);
-		System.out.println("Time in seconds: " + diffSeconds + " seconds.");
-		System.out.println("Time in minutes: " + diffMinutes + " minutes.");
-		System.out.println("Time in hours: " + diffHours + " hours.");
+		int year = Math.abs(inputDate.getYear() - currentDate.getYear());
+		int month = Math.abs(inputDate.getMonth() - currentDate.getMonth());
+		int days = Math.abs(inputDate.getDate() - currentDate.getDate());
+
+		System.out.println(
+				"Difference between input and current date is Year " + year + " Month " + month + " Days " + days);
+
 	}
 
 	public static void main(String[] args) {
 
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-//		Scanner scanner = new Scanner(System.in);
-//		Calendar calendar = Calendar.getInstance();
-//		System.out.println("Enter the date in format (dd-MM-yyyy) : ");
-//		String tempDate = scanner.nextLine();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the date in format (dd-MM-yyyy) : ");
+		String tempDate = scanner.nextLine();
 		try {
-			Date inputDate = format.parse("20-02-2020");
-			Date currentDate = format.parse("25-02-2020");
+			Date inputDate = format.parse(tempDate);
+			System.out.println("Inputed Date : " + inputDate);
+			Date currentDate = new Date();
+			System.out.println("Current Date : " + currentDate);
 			diffInDate(inputDate, currentDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 
-		//scanner.close();
+		scanner.close();
 	}
 }
