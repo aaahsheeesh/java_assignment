91352bc412994fbd9bab4331611effe52f345019
ga7uti <gauti783@gmail.com>
2020-02-08 10:18:24 +0530

    Lab 8 Completed

--- src/com/lab8/Exercise2.java
+++ src/com/lab8/Exercise2.java
@@ -1,17 +1,27 @@
 package com.lab8;
 
-class Timer implements Runnable{
+import java.time.LocalTime;
+
+class Timer implements Runnable {
 
 	@Override
 	public void run() {
-		
+
+		try {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Current Time " + LocalTime.now()); //Printing Current time after every 10sec
+				Thread.sleep(10000);
+			}
+		} catch (InterruptedException e) {
+			System.err.println("Error : " + e.getMessage());
+		}
 	}
-	
+
 }
 
-public class Exercise2  {
+public class Exercise2 {
 
 	public static void main(String[] args) {
-		
+		new Thread(new Timer()).start();
 	}
 }
